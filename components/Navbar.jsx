2549162d86347a75f42367706566bbfced1da881
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2022-09-20 17:25:15 -0700

    formatting nonsense

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Nice headphones</Link>
+        <Link href="/">AJK Woodworking</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
