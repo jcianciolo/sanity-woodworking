67aa1bb6d6239ac15ca08d28a0b224ad226505e1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2022-09-22 17:06:46 -0700

    add: updated HeroBanner styling

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,23 +7,31 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-      </div>
-      <h3>{heroBanner.midText}</h3>
-      <h1>{heroBanner.largeText1}</h1>
-      <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
 
-      <div>
+        <div>
+          <p className="beats-solo">{heroBanner.smallText}</p>
+        </div>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+      
         <Link href={`/product/${heroBanner.product}`}>
           <button type="button">{heroBanner.buttonText}</button>
         </Link>
-        <div className="desc">
-          <h5>Description</h5>
-          <p>{heroBanner.desc}</p>
-        </div>
+
       </div>
+
+      <div>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+      </div>
+
+
     </div>
   )
 }
 
 export default HeroBanner
+
+        // <div className="desc">
+        //   <h5>Description</h5>
+        //   <p>{heroBanner.desc}</p>
+        // </div>
