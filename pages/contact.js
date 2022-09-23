72df5c09a978f38cd6d524bfdda7b9b20e1a6548
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2022-09-23 12:47:37 -0700

    add: about page and contact page

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+        <div>
+            <h1>contact</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
