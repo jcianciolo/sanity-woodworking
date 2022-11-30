35f99e858e80a3214d2ec83e2526e51e309b28a6
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2022-11-29 18:08:17 -0800

    add: improving about page

--- pages/about.js
+++ pages/about.js
@@ -2,15 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="about-container">
-        <div className="about-description">
-            <h1>About me</h1>
-            <h4>Alexander Kochman graduated from U.C. Berkeley with a degree in Architecture. His passion forwoodworking has driven him to build tables of exceptional quality and character.</h4>
-        </div>
-        <div>
-            <img src="https://picsum.photos/500/400" />
-        </div>
-    </div>
+    <>
+      <div className="about-container">
+          <div className="about-description">
+              <h1>About me</h1>
+              <h4>Alexander Kochman graduated from U.C. Berkeley with a degree in Architecture. His passion forwoodworking has driven him to build tables of exceptional quality and character.</h4>
+          </div>
+          <div>
+              <img src="https://picsum.photos/500/400" />
+          </div>
+      </div>
+      <div className="about-container">
+          <div className="about-description">
+              <h1>About me</h1>
+              <h4>Alexander Kochman graduated from U.C. Berkeley with a degree in Architecture. His passion forwoodworking has driven him to build tables of exceptional quality and character.</h4>
+          </div>
+          <div>
+              <img src="https://picsum.photos/500/400" />
+          </div>
+      </div>
+    </>
   )
 }
 
