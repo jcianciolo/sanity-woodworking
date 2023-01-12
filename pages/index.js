bda42894840e6181556e5880733f8a4012bfc535
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-01-11 18:44:18 -0800

    fix: quick mess-up fixed

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,13 @@ const Home = ({ products, bannerData }) => {
 
       <div className="products-heading">
         <Image src="/../public/ajk3.jpg" height="200" width="200" />
-        <h1>AJK Woodworking</h1>
+        <h1>AJK ART & WOOD</h1>
         <h4 className="text-divider">EST 2014</h4>
         <h2>EXCEPTIONAL HANDCRAFTED TABLES AND FURNITURE</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita distinctio magnam incidunt quaerat ducimus modi similique laboriosam quam ab. Molestiae omnis quibusdam maxime quasi, autem exercitationem sit porro magnam sapiente perferendis! Eligendi minima perspiciatis earum. Neque ipsa, deserunt id quasi similique fugit aperiam inventore laudantium recusandae illo at quidem dolorem!</p>
       </div>
-      
+
+
       
       <div className="products-container">
         {products?.map((product) => <Product key={product._id} product={product}/>)} 
