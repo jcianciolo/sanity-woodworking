5117192204c3339b1a6f2ec9d55570cca45f5152
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-01-17 21:14:35 -0800

    fix: cleaned up old HeroBanner code

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
 import Slide from 'react-reveal/Slide';
-
-import { urlFor } from '../lib/client'
 
 const HeroBanner = ({ heroBanner }) => {
   return (
@@ -17,18 +14,3 @@ const HeroBanner = ({ heroBanner }) => {
 }
 
 export default HeroBanner
-
-        // <div className="desc">
-        //   <h5>Description</h5>
-        //   <p>{heroBanner.desc}</p>
-        // </div>
-
-
-
-      // <div>
-      //   <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
-      // </div>
-
-    //   <Link href={`/product/${heroBanner.product}`}>
-    //   <button type="button">{heroBanner.buttonText}</button>
-    // </Link>
