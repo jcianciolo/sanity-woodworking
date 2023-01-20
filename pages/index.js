bf39b6b8cd2c434933acba18f5eb425605015cc0
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-01-19 17:20:30 -0800

    fix: improved AJK logo colors

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ const Home = ({ products, bannerData }) => {
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
       <div className="products-heading">
-        <Image src="/../public/ajk3.jpg" height="200" width="200" />
+        <Image src="/../public/AJK4.jpg" height="200" width="200" />
         <h1>AJK ART & WOOD</h1>
         <h4 className="text-divider">EST 2014</h4>
         <h2>EXCEPTIONAL HANDCRAFTED TABLES AND FURNITURE</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita distinctio magnam incidunt quaerat ducimus modi similique laboriosam quam ab. Molestiae omnis quibusdam maxime quasi, autem exercitationem sit porro magnam sapiente perferendis! Eligendi minima perspiciatis earum. Neque ipsa, deserunt id quasi similique fugit aperiam inventore laudantium recusandae illo at quidem dolorem!</p>
+        <p>I'm Alex, a woodworker, designer and artist from the Bay Area. I specialize in customized, hand crafted wood design and furniture. My designs are conceptualized on paper, translated and designed in AutoCAD and Rhinoceros, and assembled in my woodworking studio. I love designing and assembling furniture and other goods to fit people's spaces and lifestyles. I work with solid wood and enjoy mixing and matching species for artful juxtaposition. I pick each board individually in San Francisco to hand select the most interesting parts of the tree with the most beautiful grain and color.  I'd be happy to work with you to design the piece of your dreams; whether it be art, furniture, or a mixture of the two. I guarantee if you work with me you will have the ability to explore your aesthetic preferences with the guidance of an experienced designer, woodworker, artist, and architect.</p>
       </div>
 
 
