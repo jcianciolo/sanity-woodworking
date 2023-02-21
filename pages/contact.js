4bfec827eb14106457b5800cc21e9e8f6a9598f3
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2023-02-21 14:52:04 -0800

    add: improved styling on contact form

--- pages/contact.js
+++ pages/contact.js
@@ -4,10 +4,18 @@ const Contact = () => {
   return (
     <div className="contact-container">
         <div className="contact-form">
+          <div>
             <h1>Contact Me</h1>
-            <input type='text' placeholder='text'/>
-            <input type='text' placeholder='text'/>
-            <input type='text' placeholder='text'/>
+          </div>
+          <div className='contact-inputs'>
+            <label for="Name">Name</label>
+              <input type='text' placeholder='Name'/>
+            <label for="Email">Email</label>
+              <input type='text' placeholder='Email'/>
+            <label for="Subject">Subject</label>
+              <textarea type='text' placeholder='Subject'/>
+          </div>
+          <button type="submit">Submit</button>
         </div>
         <div>
             <img src="https://picsum.photos/500/400" />
